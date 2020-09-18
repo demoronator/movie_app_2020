1707757eedd3cd270f9ce2ef362c7f6216b9951d
Wonyoung Chung <demoronator@gmail.com>
2020-09-18 15:44:14 +0900

    176p

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
   state = {
     isLoading: true,
+    movies: [],
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "Ready!"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>
+      { isLoading
+      ? "Loading..."
+      : movies.map(m => (
+        <Movie
+          key={m.id}
+          id={m.id}
+          year={m.year}
+          title={m.title}
+          summary={m.summary}
+          poster={m.medium_cover_image}
+        />
+      ))} </div>
+  }
+  
+  componentDidMount() {
+    this.getMovies();
   }
 
-  componentDidMount() {
-    // 영화 데이터 로딩
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-      console.log("aa");
-    }, 3000)
+  getMovies = async () => {
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await Axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   }
 }
 
