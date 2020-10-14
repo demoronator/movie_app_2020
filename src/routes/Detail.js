10c426f38b5798bccd9d461470d0b07f8276b1ce
Wonyoung Chung <demoronator@gmail.com>
2020-10-14 16:49:36 +0900

    Fix typo 'history' to ;React.history'

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Detail = (props) => {
-    if (props === undefined) {
-        history.push("/");
-        return "";
-    }
-    
-    console.log(props);
-    const { location, history } = props;
+  if (props === undefined) {
+    React.history.push("/");
+    return "";
+  }
 
-    if (location.state === undefined) {
-        history.push("/");
-        return "";
-    }
-    
-    return <span>{ location.state.title }</span>;
-}
+  console.log(props);
+  const { location, history } = props;
+
+  if (location.state === undefined) {
+    history.push("/");
+    return "";
+  }
+
+  return <span>{location.state.title}</span>;
+};
 
 export default Detail;
