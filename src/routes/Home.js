5725135003b14b1519042b4e617bda88533a6e60
Wonyoung Chung <demoronator@gmail.com>
2020-10-16 14:34:57 +0900

    initial commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,17 +4,45 @@ import Movie from "../components/Movie";
 import "./Home.css";
 
 const Home = () => {
+  const KEY_STORAGE = "movies";
+  const EXPIRATION_SEC = 600;
   const [isLoading, setIsLoading] = React.useState(true);
   const [movies, setMovies] = React.useState([]);
+
+  const checkValidate = (obj) => {
+    try {
+      const {
+        data: {
+          "@meta": { server_time }
+        }
+      } = obj;
+      console.log(server_time);
+      const expiration = server_time + EXPIRATION_SEC;
+      const now = Math.floor(Date.now() / 1000);
+      return now < expiration;
+    } catch (error) {
+      return false;
+    }
+  };
+
   React.useEffect(() => {
     async function getMovies() {
+      let loadedMovies = localStorage.getItem(KEY_STORAGE);
+      let parsedMovies = JSON.parse(loadedMovies);
+      if (!parsedMovies || !checkValidate(parsedMovies)) {
+        parsedMovies = await Axios.get(
+          "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+        );
+        let toSave = JSON.stringify(parsedMovies);
+        localStorage.setItem(KEY_STORAGE, toSave);
+      }
+
       const {
         data: {
           data: { movies }
         }
-      } = await Axios.get(
-        "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-      );
+      } = parsedMovies;
+
       setMovies(movies);
       setIsLoading(false);
     }
